f79f20ec8d92deba392b7f467d94aef755eadc42
João Miranda <joaopedroccb883@gmail.com>
2025-09-17 16:07:04 -0300

    Melhoria Programa

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 2)
-restaurante_praca.receber_avaliacao('Lais', 5)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
